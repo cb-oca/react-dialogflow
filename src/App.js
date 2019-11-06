9f28a0bf9c0a61c8801dec4d5165e3eed3fb022f
Corey Benson <coreybenson@iMac13.local>
2019-11-06 14:52:06 +0000

    added conditional rendering for text - bot and user classes

--- src/App.js
+++ src/App.js
@@ -12,8 +12,7 @@ class App extends Component {
       <div>
         <h1>OCA Chat!</h1>
         <ul>
-          {/* {feed.map(entry => <li>{entry.text}</li>)} */}
-          {feed.map(entry => console.log(entry))}
+          {feed.map((entry, index) => <li className={(entry.sender === 'bot' ? 'bot' : 'user')} key={index}>{entry.text}</li>)}
         </ul>
         <input type='text' onKeyDown={(e) => e.keyCode === 13?sendMessage(e.target.value) : null} />
       </div>
