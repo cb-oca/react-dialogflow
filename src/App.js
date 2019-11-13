ebb71dec37f00d4008d5a681256e9d9ef085c6e8
Corey <coreybenson@oca.ac.uk>
2019-11-13 12:55:16 +0000

    added scrollTo functionality to the chat window

--- src/App.js
+++ src/App.js
@@ -1,10 +1,29 @@
 import React, {Component} from 'react';
 import {connect} from 'react-redux';
+import { animateScroll } from "react-scroll";
 import Questions from './questions';
 import {sendMessage} from './chat';
 import styles from './App.module.scss';
 
 class App extends Component {
+  // https://codepen.io/drumilpatel/pen/aEbNMK
+
+  componentDidMount() {
+    this.scrollToBottom();
+  }
+
+  componentDidUpdate() {
+    this.scrollToBottom();
+  }
+
+  scrollToBottom() {
+    animateScroll.scrollToBottom({
+      duration: 1000,
+      delay: 0,
+      smooth: true,
+      containerId: "chat_window"
+    });
+  }
 
   render() {
     const {questions, feed, sendMessage} = this.props;
@@ -15,7 +34,7 @@ class App extends Component {
       <div className={styles.container}>
         <h1>OCA Chat!</h1>
         <Questions />
-        <ul className={styles.chat_window}>
+        <ul id="chat_window" className={styles.chat_window}>
           {feed.map((entry, index) => <li className={(entry.sender === 'bot' ? styles.bot : styles.user)} key={index}>
             <span>{entry.text}</span>
           </li>)}
